c4e7db3487dde03456eadec82cf247b103306abf
ReverseChin0 <frank.castaneda.g@gmail.com>
2019-09-19 17:53:39 -0500

    AnimacionesParaEnemigos

---
+++ Assets/Scripts/Chi-no Scripts/EnemieAgent.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemieAgent : MonoBehaviour
+{
+    public Transform[] target;
+    public NavMeshAgent agent;
+    Animator miAnim;
+
+    Vector3 currentDestination;
+    int current = 0;
+    public float Distance = 2;
+
+    private void Start()
+    {
+        currentDestination = target[current].position;
+        agent.SetDestination(currentDestination);
+        miAnim = transform.GetChild(0).GetComponent<Animator>();    
+    }
+
+    private void Update()
+    {
+        Vector3 direccion = currentDestination - transform.position;
+        if(direccion.sqrMagnitude < Distance)
+        {
+            miAnim.SetBool("moving", true);
+            current++;
+            if (current > 3)
+                current = 0;
+            print("yes");
+            currentDestination = target[current].position;
+            agent.SetDestination(currentDestination);
+        }
+    }
+}
---
+++ Assets/Scripts/Chi-no Scripts/EnemieStation.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemieStation : MonoBehaviour
+{
+    public bool Occupied = false; 
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
