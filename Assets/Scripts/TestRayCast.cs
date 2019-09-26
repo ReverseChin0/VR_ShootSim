cb8f2fc88879b9eec7190bdc1166a39781b30656
pacofmd <pacomonfmd@hotmail.com>
2019-09-26 17:04:45 -0500

    VR Shooting Mechanic OK

---
+++ Assets/Scripts/TestRayCast.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestRayCast : MonoBehaviour
+{
+
+    public Transform Barrel;
+    private void Update()
+    {
+       
+        
+        Debug.DrawRay(Barrel.position, Barrel.forward * 1000f, Color.magenta);
+    }
+
+    void Shoot()
+    {
+        //Projectile target = projectilePool.mProjectil[0];
+        /*fire.Play();
+        smoke.Play();*/
+        RaycastHit hit;
+        if (Physics.Raycast(Barrel.position, Barrel.forward, out hit, Mathf.Infinity /*rango*/))
+        {
+            Debug.DrawRay(Barrel.position, Barrel.forward * hit.distance, Color.red);
+            Debug.Log(hit.collider.name);
+        }
+
+
+        //Debug.Log("Se disparoooo");
+        // target.Disparar(this);
+    }
+}
--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -16,11 +16,13 @@ namespace Valve.VR.InteractionSystem
 
         public Transform Barrel;
 
-        public float rango = 100f;
+        public float rango = 1000f;
         public float continuarDisparando = 0f;
         public float FireRate = 10f;
-
+        public float FuerzaDeImpacto = 30f;
         public ParticleSystem fire, smoke;
+
+        public GameObject dubugObj;
 
 
 
@@ -37,7 +39,7 @@ namespace Valve.VR.InteractionSystem
             //Debug.DrawLine(Barrel.position, Barrel.position + new Vector3(0,0,Barrel.position.z+10f), Color.red);
              if (interactable.IsAttached)
              {
-                 Debug.Log("Se presiono");
+                 //Debug.Log("Se presiono");
                  if (Disparar.GetStateDown(GetComponentInParent<SteamVR_Behaviour_Pose>().inputSource) && Time.time >= continuarDisparando)
                  {
                      continuarDisparando = Time.time + 1f / FireRate;
@@ -45,11 +47,14 @@ namespace Valve.VR.InteractionSystem
                  }
              }
 
-            /*
+            
             if (Input.GetButtonDown("Fire1"))
             {
+                //Instantiate(dubugObj, Barrel);
                 Shoot();
-            }*/
+            }
+            /*Vector3 foward = Barrel.TransformDirection(Barrel.forward) * 10f;
+            Debug.DrawRay(Barrel.position, foward, Color.magenta);*/
         }
 
         void Shoot()
@@ -58,13 +63,23 @@ namespace Valve.VR.InteractionSystem
             fire.Play();
             smoke.Play();
             RaycastHit hit;
-            if (Physics.Raycast(Barrel.position, Barrel.forward, out hit, Mathf.Infinity /*rango*/))
+            if (Physics.Raycast(Barrel.position, Barrel.forward, out hit, /*Mathf.Infinity*/rango))
             {
-                Debug.DrawRay(Barrel.position, Barrel.forward, Color.red);
-                Debug.Log(hit.collider.name);                
-            }           
+                Debug.DrawRay(Barrel.position, Barrel.TransformDirection(Vector3.forward) * hit.distance, Color.green);
+                Debug.Log("Le pegaste a: "+hit.collider.name);
+                if(hit.rigidbody != null)
+                {
+                    hit.rigidbody.AddForce(-hit.normal * FuerzaDeImpacto);
+                }
+            }
+            else
+            {
+                Debug.DrawRay(Barrel.position, Barrel.TransformDirection(Vector3.forward) * 100f, Color.red);
+                Debug.Log("No le pegaste");
+            }        
 
-            Debug.Log("Se disparoooo");
+            
+            //Debug.Log("Se disparoooo");
            // target.Disparar(this);
         }
 
